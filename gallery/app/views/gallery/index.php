33a57a5f49f4c4da797e7170b5a6c92dc9ed7a8c
refirio <refirio@gmail.com>
2026-02-13 23:24:46 +0900

    HTMLのインデントを調整

--- gallery/app/views/gallery/index.php
+++ gallery/app/views/gallery/index.php
@@ -1,33 +1,33 @@
 <?php import('app/views/header.php') ?>
 
-            <div id="plugin-gallery">
-                <h2 class="h3 mb-3">Gallery</h2>
-                <?php e($GLOBALS['plugin']['gallery']['setting']['text_index']) ?>
-                <?php foreach ($_view['categories'] as $category) : ?>
-                <?php if (isset($category['name'])) : ?><h3 class="h4 mb-4"><?php h($category['name']) ?></h3><?php endif ?>
-                <?php if (!empty($_view['entry_sets'][$category['id']])) : ?>
-                <div class="row">
-                    <?php foreach ($_view['entry_sets'][$category['id']] as $entry) : ?>
-                    <div class="col-md-3">
-                        <div class="card mb-4 shadow-sm">
-                            <a href="<?php t(MAIN_FILE) ?>/gallery/detail/<?php h($entry['code']) ?>">
-                                <?php if (!empty($entry['thumbnail'])) : ?>
-                                <img class="card-img-top" src="<?php t($GLOBALS['config']['storage_url'] . '/' . $GLOBALS['config']['file_target']['entry'] . $entry['id'] . '/' . $entry['thumbnail']) ?>" alt="<?php h($entry['title']) ?>">
-                                <?php endif ?>
-                            </a>
-                            <div class="card-body">
-                                <p class="card-text"><a href="<?php t(MAIN_FILE) ?>/gallery/detail/<?php h($entry['code']) ?>"><?php h($entry['title']) ?></a></p>
-                            </div>
-                        </div>
+    <div id="plugin-gallery">
+        <h2 class="h3 mb-3">Gallery</h2>
+        <?php e($GLOBALS['plugin']['gallery']['setting']['text_index']) ?>
+        <?php foreach ($_view['categories'] as $category) : ?>
+        <?php if (isset($category['name'])) : ?><h3 class="h4 mb-4"><?php h($category['name']) ?></h3><?php endif ?>
+        <?php if (!empty($_view['entry_sets'][$category['id']])) : ?>
+        <div class="row">
+            <?php foreach ($_view['entry_sets'][$category['id']] as $entry) : ?>
+            <div class="col-md-3">
+                <div class="card mb-4 shadow-sm">
+                    <a href="<?php t(MAIN_FILE) ?>/gallery/detail/<?php h($entry['code']) ?>">
+                        <?php if (!empty($entry['thumbnail'])) : ?>
+                        <img class="card-img-top" src="<?php t($GLOBALS['config']['storage_url'] . '/' . $GLOBALS['config']['file_target']['entry'] . $entry['id'] . '/' . $entry['thumbnail']) ?>" alt="<?php h($entry['title']) ?>">
+                        <?php endif ?>
+                    </a>
+                    <div class="card-body">
+                        <p class="card-text"><a href="<?php t(MAIN_FILE) ?>/gallery/detail/<?php h($entry['code']) ?>"><?php h($entry['title']) ?></a></p>
                     </div>
-                    <?php endforeach ?>
                 </div>
-                <?php elseif (isset($category['name'])) : ?>
-                <div class="row">
-                    <p>登録されていません。</p>
-                </div>
-                <?php endif ?>
-                <?php endforeach ?>
             </div>
+            <?php endforeach ?>
+        </div>
+        <?php elseif (isset($category['name'])) : ?>
+        <div class="row">
+            <p>登録されていません。</p>
+        </div>
+        <?php endif ?>
+        <?php endforeach ?>
+    </div>
 
 <?php import('app/views/footer.php') ?>
